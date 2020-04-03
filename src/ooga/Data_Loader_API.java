5f93ae4f9638c5efcabf780a8e44f5b491d9bc07
Guangyu Feng <guangyu.feng@duke.edu>
2020-04-03 12:14:18 +0800

    change api

--- src/ooga/Data_Loader_API.java
+++ src/ooga/Data_Loader_API.java
@@ -1,42 +1,42 @@
 package ooga;
 
-import javax.swing.text.html.ImageView;
-import java.util.List;
+import javafx.scene.image.Image;
+import ooga.model.interfaces.Weapon;
+import ooga.model.interfaces.gameMap.Cell;
+
 import java.util.Map;
 
 /**
  * The interface for game loader
  */
 public interface Data_Loader_API {
-    /**
-     * Load the map of single game level
-     * @param levelName
-     * @return
-     */
-    GameMapGraph loadLevel(double levelName);
+
+    Cell loadCell(int row, int col);
 
     /**
-     * load text files from the database. Same concern as loadGameIcon.
-     * @return
+     * load text files from the database. Keyword specifies one piece of data out of a category. Category can be Dialog content
+     *
      */
-    List<String> loadText();
+    String loadText(String keyword, String category);
 
-    /**
-     * Load the game's status and string is the name of the status while integer is the value
-     * @return
-     */
+    Character loadCharacter(int ID);
+
+    Weapon loadWeapon(int ID);
+
     Map<String, Integer> loadGameStatus();
 
-    /**
-     * I am concerned about this way because list will not tell you which imageView is which.
-     * @return list of icons
-     */
-    List<ImageView> loadGameIcon();
 
+    Image loadImage(String keyword, String category);
+
+    
     /**
      * Load the player's preference and string is the name of the preference while integer is the value
      * @return
      */
     Map<String, Integer>  loadPlayerPreference();
+
+    Integer loadInteger(String keyword, String category);
+
+
 
 }
