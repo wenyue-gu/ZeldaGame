211afe3295bde5a20ea7cbd6bb63ddd4618d03c4
Cady Zhou <ziqing.zhou@duke.edu>
2020-04-02 21:06:00 -0400

    complete alive interface and comments

--- src/ooga/model/interfaces/Alive.java
+++ src/ooga/model/interfaces/Alive.java
@@ -1,5 +1,40 @@
 package ooga.model.interfaces;
 
+/**
+ * Objects that implement this interface have certain HP values. HP can be deducted under certain
+ * circumstances (for example, being attacked). This object "dies" if HP is equal to 0. HP cannot be smaller than 0.
+ *
+ * @author cady
+ */
 public interface Alive {
 
+  /**
+   * Gets the HP of this object
+   * @return  the HP of this object
+   */
+  int getHP();
+
+  /**
+   * Sets the HP of this object
+   * @param hp  the HP to set
+   */
+  void setHP(int hp);
+
+  /**
+   * Adds HP by a certain amount
+   * @param deltaHP the HP to add
+   */
+  void addHP(int deltaHP);
+
+  /**
+   * Subtracts HP by a certain amount
+   * @param deltaHP   the HP to subtract
+   */
+  void subtractHP(int deltaHP);
+
+  /**
+   * Gets if this object is still alive
+   * @return  if the HP is larger than 0
+   */
+  boolean isAlive();
 }
