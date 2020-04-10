0add79e43088eb2193de26b0b3bc7cc39d386820
Cady Zhou <ziqing.zhou@duke.edu>
2020-04-10 01:21:45 -0400

    implement zeldaplayer

--- src/ooga/model/characters/ZeldaPlayer.java
+++ src/ooga/model/characters/ZeldaPlayer.java
@@ -2,6 +2,42 @@ package ooga.model.characters;
 
 import ooga.model.interfaces.Scorable;
 
-public abstract class ZeldaPlayer extends ZeldaCharacter {
+public class ZeldaPlayer extends ZeldaCharacter implements Scorable {
 
+  protected double score;
+  private boolean isScoring;
+
+  public ZeldaPlayer(int initialHp) {
+    super(initialHp);
+  }
+
+  public ZeldaPlayer(int initialHp, int weapon) {
+    super(initialHp, weapon);
+  }
+
+  public ZeldaPlayer(int initialHp, int weapon, int attack) {
+    super(initialHp, weapon, attack);
+    isScoring = true;
+    score = 0;
+  }
+
+  @Override
+  public boolean isScoring() {
+    return isScoring;
+  }
+
+  @Override
+  public double getScore() {
+    return score;
+  }
+
+  @Override
+  public void setScore(double score) {
+    this.score = score;
+  }
+
+  @Override
+  public void addScore(double deltaScore) {
+    score += deltaScore;
+  }
 }
