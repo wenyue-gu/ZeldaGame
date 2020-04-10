38cc19d262e176c492e80fc5976d047e051ed5fe
Cady Zhou <ziqing.zhou@duke.edu>
2020-04-09 22:44:15 -0400

    implement classes for movements

--- src/ooga/model/move/JumpableObject.java
+++ src/ooga/model/move/JumpableObject.java
@@ -1,8 +1,54 @@
-package ooga.model.move;
+package ooga.model.characters;
 
+import ooga.model.enums.MovingState;
 import ooga.model.interfaces.movables.Jumpable;
 
-public abstract class JumpableObject extends MovingObject1D implements Jumpable {
+public class Player1DJumpable extends Movable1D implements Jumpable {
+
+  public static final int DEFAULT_Z_SPEED = 5;
+  protected double zSpeed;
+
+  public Player1DJumpable() {
+    this(DEFAULT_X, DEFAULT_Z_SPEED);
+  }
+
+  public Player1DJumpable(double x, double zSpeed) {
+    this(x, DEFAULT_X_SPEED, zSpeed);
+  }
+
+  public Player1DJumpable(double x, double xSpeed, double zSpeed) {
+    super(x, xSpeed);
+    this.zSpeed = zSpeed;
+    movingState = MovingState.STOP;
+  }
 
 
+  @Override
+  public void setZSpeed(double zSpeed) {
+    this.zSpeed = zSpeed;
+  }
+
+  @Override
+  public double getZSpeed(double zSpeed) {
+    return zSpeed;
+  }
+
+  @Override
+  public void jump() {
+    setToJump();
+    notifyAnimation();
+  }
+
+  private void notifyAnimation() {
+  }
+
+  @Override
+  public void setToJump() {
+    movingState = MovingState.JUMP;
+  }
+
+  @Override
+  public void setNotToJump() {
+    movingState = MovingState.STOP;
+  }
 }
--- src/ooga/model/move/MovingObject2D.java
+++ src/ooga/model/move/MovingObject2D.java
@@ -1,7 +1,92 @@
-package ooga.model.move;
+package ooga.model.characters;
 
 import ooga.model.interfaces.movables.Movable2D;
 
-public abstract class MovingObject2D extends MovingObject1D implements Movable2D {
+public class Player2D extends Movable1D implements Movable2D {
+  public static final int DEFAULT_Y_SPEED = 5;
+  public static final int DEFAULT_Y = 0;
 
+  protected double y;
+  protected double ySpeed;
+  protected double diagonalSpeed;
+
+  protected boolean canMoveDiagonally;
+
+  public Player2D() {
+    this(DEFAULT_X, DEFAULT_Y);
+  }
+
+  public Player2D(double x, double y) {
+    this(x, y, DEFAULT_X_SPEED, DEFAULT_Y_SPEED);
+  }
+
+  public Player2D(double x, double y, double xSpeed, double ySpeed) {
+    super(x, xSpeed);
+    this.y = y;
+    this.ySpeed = ySpeed;
+    canMoveDiagonally = false;
+    diagonalSpeed = 0;
+  }
+
+  @Override
+  public void setY(double y) {
+    this.y = y;
+  }
+
+  @Override
+  public double getY() {
+    return y;
+  }
+
+  @Override
+  public void moveInY(double deltaY) {
+    y += deltaY;
+  }
+
+  @Override
+  public void setYSpeed(double ySpeed) {
+    this.ySpeed = ySpeed;
+  }
+
+  @Override
+  public double getYSpeed() {
+    return ySpeed;
+  }
+
+  @Override
+  public void setSpeed(double xSpeed, double ySpeed) {
+    this.xSpeed = xSpeed;
+    this.ySpeed = ySpeed;
+  }
+
+  @Override
+  public void set2DPos(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  @Override
+  public void enableDiagonalMove() {
+    canMoveDiagonally = true;
+  }
+
+  @Override
+  public void disableDiagonalMove() {
+    canMoveDiagonally = false;
+  }
+
+  @Override
+  public void setsDiagonalSpeed(double dSpeed) {
+    diagonalSpeed = dSpeed;
+  }
+
+  @Override
+  public double getDiagonalSpeed() {
+    return diagonalSpeed;
+  }
+
+  @Override
+  public boolean isDiagonalMove() {
+    return canMoveDiagonally;
+  }
 }
