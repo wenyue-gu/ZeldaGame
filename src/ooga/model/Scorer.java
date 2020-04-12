5ae905eddc0b127a8b8821611de3f1e3f0c92237
Cady Zhou <ziqing.zhou@duke.edu>
2020-04-12 10:23:32 -0400

    make scoreer its own class

--- src/ooga/model/Scorer.java
+++ src/ooga/model/Scorer.java
@@ -1,7 +1,42 @@
 package ooga.model;
 
-import javax.swing.Scrollable;
+import ooga.model.interfaces.Scorable;
 
-public abstract class Scorer implements Scrollable {
+public class Scorer implements Scorable {
+  protected double score;
+  private boolean isScoring;
+
+  public Scorer() {
+    this(0);
+  }
+
+  public Scorer(double score) {
+    this(score, true);
+  }
+
+  public Scorer(double score, boolean isScoring) {
+    this.score = score;
+    this.isScoring = isScoring;
+  }
+
+  @Override
+  public boolean isScoring() {
+    return isScoring;
+  }
+
+  @Override
+  public double getScore() {
+    return score;
+  }
+
+  @Override
+  public void setScore(double score) {
+    this.score = score;
+  }
+
+  @Override
+  public void addScore(double deltaScore) {
+    score += deltaScore;
+  }
 
 }
